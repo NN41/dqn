dab530183e7bbb7cf6cc9a39a8e53789dcd04f20
NN41 <nicknauta9@gmail.com>
2025-06-24 14:03:51 +0200

    Implemented CNN Q-network

--- src/networks.py
+++ src/networks.py
@@ -2,44 +2,21 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class PolicyMLP(nn.Module):
-    """
-    A densely-connected network representing the policy.
-    Has one hidden layer with ReLU activation and outputs logits.
-    """
-    def __init__(self, input_size, hidden_size, output_size):
+class QNetwork(nn.Module):
+    def __init__(self, num_actions):
         super().__init__()
 
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
+        self.conv1 = nn.Conv2d(in_channels=4, out_channels=16, kernel_size=8, stride=4) # for input (H, W) = (84, 84), gives output (20, 20)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2) # for input (H, W) = (20, 20), gives output (9, 9)
+        self.fc1 = nn.Linear(in_features=32*9*9, out_features=256)
+        self.fc2 = nn.Linear(in_features=256, out_features=num_actions)
 
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, output_size)
-
-    def forward(self, x):
-        h = F.relu(self.fc1(x))
-        logits = self.fc2(h)
+    def forward(self, x): # input must be of shape (batch_size, 4, 84, 84) or (4, 84, 84)
+        h1 = F.relu(self.conv1(x))
+        h2 = F.relu(self.conv2(h1))
+        h2_flat = torch.flatten(h2, start_dim=-3)
+        h3 = F.relu(self.fc1(h2_flat))
+        logits = self.fc2(h3)
         return logits
-    
-class ValueMLP(nn.Module):
-    """
-    A densely-conncted network representing the value function.
-    Has one hidden layer with ReLU activation and outputs a scalar.
-    """
-    def __init__(self, input_size, hidden_size, output_size):
-        super().__init__()
-
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, output_size)
-
-    def forward(self, x):
-        h = F.relu(self.fc1(x))
-        output = self.fc2(h)
-        return output
  
  
