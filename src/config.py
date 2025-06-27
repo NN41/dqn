c82dce97a243bc3294a966936b6f995a1493e593
NN41 <nicknauta9@gmail.com>
2025-06-27 16:27:41 +0200

    Implmented hyperparameters as config dataclass

--- src/config.py
+++ src/config.py
@@ -5,50 +5,85 @@ from typing import Literal, Optional
 @dataclass
 class Config:
     """
-    Configuration object for the training and experimentation pipeline.
-
-    This class contains hyperparameters and settings for the agent, as well as the training process.
+    Configuration class containing hyperparameters and settings for the agent and training process.
     An instance of this class acts as the single source of truth for a given run and can be
     serialized for logging and reproducibility purposes of experiments.
     """
+    # Training loop
+    num_samples_per_epoch: int = 10_000 
+    num_epochs: int = 64
+    seed: int = 42
     
-    # --- System and environment ---
+    @property
+    def num_samples(self) -> int:
+        return self.num_samples_per_epoch * self.num_epochs
+
+    # Network and training and testing
+    target_network_sync_interval: int = 500
+    use_target_network: bool = True
+    gamma: float = 0.99
+    num_test_frames: int = 2_000
+    learning_rate: float = 1e-4
+
+    # Epsilon annealing
+    eps_start: float = 1.0
+    eps_end: float = 0.1
+
+    @property
+    def anneal_length(self) -> float:
+        return self.num_samples * 0.5
+
+    # Replay memory and preprocessing
+    capacity: int = 25_000
+    num_frames_in_stack: int = 4
+    minibatch_size: int = 32
+
+    # Saving, info and logging
+    update_interval: int = 5_000 # number of environment steps between updates about training process
+    checkpoint_interval: int = 10_000 # number of environment steps between creating a checkpoint of the trained network
+    save_dir: str  = "models/checkpoints"
+
+    # Other
     device: str = field(default_factory=lambda: "cuda" if torch.cuda.is_available() else "cpu")
-    env_name: str = "CartPole-v1"
 
-    # --- Training loop hyperparameters ---
-    num_episodes: int = 20
-    num_epochs_policy_network: int = 50 
-    num_epochs_value_network: int = 1
 
-    # --- Policy network hyperparameters ---
-    policy_hidden_size: int = 4
-    policy_learning_rate: float = 0.01
+    # # --- System and environment ---
+    # device: str = field(default_factory=lambda: "cuda" if torch.cuda.is_available() else "cpu")
+    # env_name: str = "CartPole-v1"
 
-    # --- Value function network hyperparameters ---
-    value_hidden_size: int = 20
-    value_learning_rate: float = 0.01
-    value_weight_decay: float = 0
+    # # --- Training loop hyperparameters ---
+    # num_episodes: int = 20
+    # num_epochs_policy_network: int = 50 
+    # num_epochs_value_network: int = 1
 
-    # --- Algorithm hyperparameters ---
-    # GAE-specific parameters
-    gamma_gae: float = 0.98 # Equivalent to standard discount factor
-    lambda_gae: float = 0.96 # GAE-specific parameter for bias-variance tradeoff
+    # # --- Policy network hyperparameters ---
+    # policy_hidden_size: int = 4
+    # policy_learning_rate: float = 0.01
 
-    # Defines the weight for the log-probabilities in the policy gradient estimate.
-    # 'r': full returns, 'dfr': discounted future returns, 'fr': future returns (rewards-to-go),
-    # 'gae': generalized advantage estimates, 'td': temporal difference errors, 'dfrb': discounted future returns - value function baseline
-    weight_kind: Literal['r', 'fr', 'dfr', 'gae', 'td', 'dfrb'] = 'fr'
+    # # --- Value function network hyperparameters ---
+    # value_hidden_size: int = 20
+    # value_learning_rate: float = 0.01
+    # value_weight_decay: float = 0
 
-    # Defines the averaging method for the policy gradient estimate
-    # 'a': average over all episodes, 't': average over trajectories (i.e., per episode)
-    avg_kind: Literal['a', 't'] = 'a'
+    # # --- Algorithm hyperparameters ---
+    # # GAE-specific parameters
+    # gamma_gae: float = 0.98 # Equivalent to standard discount factor
+    # lambda_gae: float = 0.96 # GAE-specific parameter for bias-variance tradeoff
 
-    # --- Logging and experimentation metadata ---
-    render_every_n_epochs: int = 5
-    log_params_every_n_epochs: int = 5 # When to log the gradient (norms) and parameter (norms) of the policy and value networks
-    log_dir: Optional[str] = None  # Directory for logging results, to be used by TensorBoard and populated at runtime
-    experiment_name: Optional[str] = None # Tag for the run, to be used for logging and experiment tracking, populated at runtime
-    seed: Optional[int] = None
+    # # Defines the weight for the log-probabilities in the policy gradient estimate.
+    # # 'r': full returns, 'dfr': discounted future returns, 'fr': future returns (rewards-to-go),
+    # # 'gae': generalized advantage estimates, 'td': temporal difference errors, 'dfrb': discounted future returns - value function baseline
+    # weight_kind: Literal['r', 'fr', 'dfr', 'gae', 'td', 'dfrb'] = 'fr'
+
+    # # Defines the averaging method for the policy gradient estimate
+    # # 'a': average over all episodes, 't': average over trajectories (i.e., per episode)
+    # avg_kind: Literal['a', 't'] = 'a'
+
+    # # --- Logging and experimentation metadata ---
+    # render_every_n_epochs: int = 5
+    # log_params_every_n_epochs: int = 5 # When to log the gradient (norms) and parameter (norms) of the policy and value networks
+    # log_dir: Optional[str] = None  # Directory for logging results, to be used by TensorBoard and populated at runtime
+    # experiment_name: Optional[str] = None # Tag for the run, to be used for logging and experiment tracking, populated at runtime
+    # seed: Optional[int] = None
  
  
