b8dde8cfb63725b69d0092aacb5c1eaee9719af0
NN41 <nicknauta9@gmail.com>
2025-06-27 16:29:02 +0200

    Code cleanup

--- src/networks.py
+++ src/networks.py
@@ -3,7 +3,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class QNetwork(nn.Module):
-    def __init__(self, num_actions):
+    """
+    Implements Deep Q-Network using two convolutional layers followed by two fully-connected layers.
+
+    Args:
+        num_actions (int): Number of possible actions the agent can take.
+    """
+    def __init__(self, num_actions: int):
         super().__init__()
 
         self.conv1 = nn.Conv2d(in_channels=4, out_channels=16, kernel_size=8, stride=4) # for input (H, W) = (84, 84), gives output (20, 20)
@@ -11,12 +17,12 @@ class QNetwork(nn.Module):
         self.fc1 = nn.Linear(in_features=32*9*9, out_features=256)
         self.fc2 = nn.Linear(in_features=256, out_features=num_actions)
 
-    def forward(self, x): # input must be of shape (batch_size, 4, 84, 84) or (4, 84, 84)
+    def forward(self, x: torch.Tensor) -> torch.Tensor: # input must be of shape (batch_size, 4, 84, 84) or (4, 84, 84)
         h1 = F.relu(self.conv1(x))
         h2 = F.relu(self.conv2(h1))
         h2_flat = torch.flatten(h2, start_dim=-3)
         h3 = F.relu(self.fc1(h2_flat))
-        output = self.fc2(h3) # these are not logits (unnormalized probabilities), they are Q-values for each action
+        output = self.fc2(h3) # these are not logits, they are predicted Q-values for each individual action
         return output
  
  
