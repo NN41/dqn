b96efd88ca5fceaefe767e190b92155154e63bac
NN41 <87565186+NN41@users.noreply.github.com>
2025-07-09 16:47:18 +0200

    Remove unused and commented-out code

--- src/render.py
+++ src/render.py
@@ -1,5 +1,3 @@
-# %%
-
 import gymnasium as gym
 import ale_py
 import os
@@ -10,7 +8,6 @@ from src.networks import QNetwork
 gym.register_envs(ale_py)
 
 env = gym.make("ALE/Pong-v5")
-# env = gym.make("ALE/Pong-v5", render_mode="human")
 
 model_dir = "models/trained"
 model_name = "trained_base_and_target_networks.pth"
@@ -23,16 +20,4 @@ dqn.load_state_dict(state_dict["model_state"])
 obs, info = env.reset()
 for _ in range(100):
     pass
-# def render_episode(q_network = None, seed = None):
-#     env = gym.make("ALE/Pong-v5", render_mode="human")
-#     obs, info = env.reset()
-#     for _ in range(1000):
-#         if q_network not None:
-        
-#         else:
-#             act = env.action_space.sample()
-#         obs, rew, term, trunc, info = env.step(act)
-#         if term or trunc:
-#             break
-#     env.close()
 
