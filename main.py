cda0414fb3d34f94a8a3a8acc59955185d71082c
NN41 <nicknauta9@gmail.com>
2025-07-09 17:53:33 +0200

    Updated main

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 from src.config import Config
 from src.trainer import Trainer
 
-
-def main() -> None:
-    """Entry point for training."""
-    config = Config(num_test_frames=5, num_samples_per_epoch=100, num_epochs=10)
-    trainer = Trainer(config)
-    trainer.train()
-
-
-if __name__ == "__main__":
-    main()
-
+config = Config(num_test_frames=5, num_samples_per_epoch=100, num_epochs=10)
+trainer = Trainer(config)
+trainer.train()
